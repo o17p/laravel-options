bed9cbeb212371ea97e98f655c6d60030898ced7
Flemming Westphal <flemming@o17p.com>
2025-05-13 16:39:30 +0200

    Remove unnecessary comment from register method in OptionsServiceProvider

--- src/OptionsServiceProvider.php
+++ src/OptionsServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class OptionsServiceProvider extends ServiceProvider {
     public function register(): void {
-        // Bindings if needed
     }
 
     public function boot(): void {
